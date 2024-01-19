457a983b70b23736b50d57f72a406804d960f746
atulsubedi <atul.work15@gmail.com>
2024-01-19 20:16:28 +0545

    Added TestCase For getURLSFromHTML

--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,15 @@
-const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { getURLSFromHTML } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
 
-test('to convert the url', () => {
-    expect(normalizeURL('https://atulsubedi.com.np/path/')).toBe('atulsubedi.com.np/path');
-});
+ test('normalize url', () => {
+     expect(normalizeURL('https://ATULsubedi.com.np/path/')).toEqual('atulsubedi.com.np/path');
+ });
+
+test('get url', () => {
+    expect(getURLSFromHTML('<a href="https://blog.boot.dev"></a>', 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev']);
+})
+test('url', () => {
+    expect(getURLSFromHTML('<a href="/xyz"></a>', 'https://blog.boot.dev')).toEqual(['https://blog.boot.dev/xyz']);
+})
